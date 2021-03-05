01fc68fac158945030550b6eb9cc6a744b8c5281
Gastón Martínez <gastongmartinez@gmail.com>
2021-03-05 19:58:01 -0300

    Commit inicial

---
+++ gnome_config.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+R_USER=`id -u`
+if [ "$R_USER" -eq 0 ];
+then
+   echo "Este script debe usarse con un usuario regular."
+   echo "Saliendo..."
+   exit 1
+fi
+
+if [ -z $DISPLAY ];
+then
+    echo -e "Debe ejecutarse dentro del entorno grafico.\n"
+    echo "Saliendo..."
+    exit 2
+fi
+
+############################################## Extensiones ##################################################################################
+# User themes
+dconf write /org/gnome/shell/enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.  github.com','user-theme@gnome-shell-extensions.gcampax.github.com']"
+
+# ArcMenu
+dconf write /org/gnome/shell/enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.  github.com','user-theme@gnome-shell-extensions.gcampax.github.com', 'arcmenu@arcmenu.com']"
+dconf write /org/gnome/shell/extensions/arcmenu/available-placement "[true, false, false]"
+dconf write /org/gnome/mutter/overlay-key "'Super_L'"
+dconf write /org/gnome/desktop/wm/keybindings/panel-main-menu "['<Alt>F1']"
+dconf write /org/gnome/shell/extensions/arcmenu/pinned-app-list "['Web', '', 'org.gnome.Epiphany.desktop', 'Terminal', '', 'orggnome.Terminal. desktop', 'ArcMenu Settings', 'ArcMenu_ArcMenuIcon', 'gnome-extensions prefs arcmenu@arcmenu.com']"
+dconf write /org/gnome/shell/extensions/arcmenu/menu-hotkey "'Super_L'"
+
+# Dash to Dock
+dconf write /org/gnome/shell/enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.  github.com','user-theme@gnome-shell-extensions.gcampax.github.com', 'arcmenu@arcmenu.com', 'dash-to-dock@micxgx.gmail.com']"
+dconf write /org/gnome/shell/extensions/arcmenu/available-placement "[false, false, true]"
+dconf write /org/gnome/shell/extensions/dash-to-dock/preferred-monitor 0
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
+dconf write /org/gnome/shell/extensions/dash-to-dock/autohide true
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button false
+dconf write /org/gnome/shell/extensions/dash-to-dock/transparency-mode "'DYNAMIC'"
+dconf write /org/gnome/shell/extensions/dash-to-dock/customize-alphas true
+dconf write /org/gnome/shell/extensions/dash-to-dock/min-alpha 0.10
+dconf write /org/gnome/shell/extensions/dash-to-dock/max-alpha 0.60
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button true
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-apps-at-top true
+dconf write /org/gnome/shell/extensions/dash-to-dock/animate-show-apps true
+
+# Drop down Terminal
+dconf write /org/gnome/shell/enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.  github.com','user-theme@gnome-shell-extensions.gcampax.github.com', 'arcmenu@arcmenu.com', 'dash-to-dock@micxgx.gmail.com','drop-down-terminal-x@bigbn.pro']"
+dconf write /pro/bigbn/drop-down-terminal-x/other-shortcut "['F12']"
+dconf write /pro/bigbn/drop-down-terminal-x/enable-tabs true
+dconf write /pro/bigbn/drop-down-terminal-x/use-default-colors true
+#############################################################################################################################################
+# Teclado
+#dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'es+winkeys')]"
+
+# Ventanas
+dconf write /org/gnome/desktop/wm/preferences/button-layout "'appmenu:minimize,maximize,close'"
+dconf write /org/gnome/mutter/center-new-windows true
+
+# Tema
+dconf write /org/gnome/desktop/interface/gtk-theme "'Prof-Gnome-Dark-3.6'"
+dconf write /org/gnome/shell/extensions/user-theme/name "'Prof-Gnome-Dark-3.6'"
+dconf write /org/gnome/desktop/interface/cursor-theme "'Qogir-manjaro-dark'"
+dconf write /org/gnome/desktop/interface/icon-theme "'Qogir-manjaro-dark'"
+
+# Wallpaper
+dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/wallpapers/Space/space%2018.jpg'"
+
+# Establecer fuentes
+dconf write /org/gnome/desktop/interface/font-name "'Noto Sans CJK HK 11'"
+dconf write /org/gnome/desktop/interface/document-font-name "'Noto Sans CJK HK 11'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'Monospace 10'"
+dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Noto Sans CJK HK Bold 11'"
+
+# Aplicaciones favoritas
+dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Boxes.desktop', 'org.gnome.Evolution.desktop', 'libreoffice-calc.desktop', 'chromium-browser.desktop', 'firefox.desktop', 'brave-browser.desktop', 'org.qbittorrent.qBittorrent.desktop', 'code.desktop', 'codeblocks.desktop', 'Alacritty.desktop', 'clementine.desktop', 'vlc.desktop', 'org.gnome.tweaks.desktop']"
+
+# Suspender
+# En 2 horas enchufado
+dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-timeout 7200
+# En 30 minutos con bateria
+dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-battery-timeout 1800
+#############################################################################################################################################
+
+
+# Autostart Apps
+#if [ ! -d ~/.config/autostart ]; then
+#    mkdir -p ~/.config/autostart
+#fi
+#cp /usr/share/applications/plank.desktop ~/.config/autostart/    
+
+# Doom Emacs
+cd ~
+if [ -d ~/.emacs.d ]; then
+    rm -Rf ~/.emacs.d
+fi
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+# ZSH
+if [ ! -d ~/.local/share/zsh ]; then
+    mkdir ~/.local/share/zsh
+fi
+touch ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/zsh/powerlevel10k
+echo 'source ~/.local/share/zsh/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo 'source /usr/share/autojump/autojump.zsh' >>~/.zshrc
+echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
+echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >>~/.zshrc
+echo 'exec zsh' >>~/.bashrc
---
+++ install.sh
@@ -0,0 +1,190 @@
+#!/usr/bin/env bash
+
+dnf update -y
+
+systemctl enable sshd
+
+# Validacion del usuario ejecutando el script
+R_USER=`id -u`
+if [ "$R_USER" -ne 0 ];
+then
+    echo -e "\nDebe ejecutar este script como root o utilizando sudo.\n"
+    exit 1
+fi
+
+# RPMFusion
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+# Repositorio VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+
+# Brave
+dnf install dnf-plugins-core -y
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+############################### Apps Generales ################################
+PAQUETES=(
+    #### Powermanagement ####
+    'tlp'
+    'powertop'
+
+    #### Gnome ####
+    'gnome-tweaks'
+    'gnome-shell-extension-dash-to-dock'
+    'gnome-shell-extension-pop-shell'
+    'gnome-shell-extension-user-theme'
+
+    #### Fuentes ####
+    'terminus-fonts'
+    'fontawesome-fonts'
+    'cascadia-code-fonts'
+
+    #### WEB ####
+    'chromium'
+    'thunderbird'
+    'remmina'
+    'qbittorrent'
+    'brave-browser'
+    'evolution'
+
+    #### Shells ####
+    'fish'
+    'zsh'
+    'zsh-autosuggestions'
+    'zsh-syntax-highlighting'
+    'dialog'
+    'autojump'
+    'autojump-fish'
+    'autojump-zsh'
+
+    #### Archivos ####
+    'mc'
+    'doublecmd-gtk'
+    'vifm'
+    'meld'
+    'stow'
+    'ripgrep'
+    'exfatprogs'
+    'autofs'
+
+    #### Sistema ####
+    'p7zip'
+    'alacritty'
+    'conky'
+    'conky-manager'
+    'htop'
+    'bashtop'
+    'neofetch'
+    'lshw'
+    'lshw-gui'
+    'powerline'
+    'neovim'
+    'emacs'
+
+    #### Multimedia ####
+    'clementine'
+    'vlc'
+    'python-vlc'
+    'mpv'
+
+    #### Juegos ####
+    'chromium-bsu'
+
+    #### Redes ####
+    'nmap'
+    'wireshark'
+
+    #### Diseño ####
+    'gimp'
+    'inkscape'
+    'krita'
+    'blender'
+    'freecad'
+
+    #### DEV ####
+    'clang'
+    'codeblocks'
+    'filezilla'
+    'golang'
+    'rust'
+    'java-latest-openjdk'
+    'code'
+
+    #### WM ####
+    'qtile'
+)
+ 
+for PAQ in "${PAQUETES[@]}"; do
+    dnf install "$PAQ" -y
+done
+###############################################################################
+
+########################## Virtualizacion #####################################
+read -p "Instalar virtualizacion? (S/N): " VIRT
+if [ "$VIRT" == 'S' ]; then
+    VIRTPKGS=(
+        'virt-manager'
+        'qemu-kvm'
+        'edk2-ovmf'
+        'ebtables-services'
+        'dnsmasq'
+        'bridge-utils'
+    )
+    for PAQ in "${VIRTPKGS[@]}"; do
+        dnf install "$PAQ" -y
+    done
+fi
+###############################################################################
+
+################################ Wallpapers #####################################
+read -p "Instalar Wallpapers? (S/N): " WPP
+if [ "$WPP" == 'S' ]; then
+    echo -e "\nInstalando wallpapers..."
+    git clone https://github.com/gastongmartinez/wallpapers.git
+    mv -f wallpapers/ "/usr/share/backgrounds/"
+fi
+#################################################################################
+
+################################## Iconos #######################################
+read -p "Instalar iconos? (S/N): " IC
+if [ "$IC" == 'S' ]; then
+    echo -e "\nInstalando iconos...\n"
+    for ICON in ./Iconos/*.xz
+    do
+        tar -xf "$ICON" -C /usr/share/icons/
+    done
+fi
+#################################################################################
+
+################################ Temas GTK ######################################
+read -p "Instalar temas de escritorio? (S/N): " TM
+if [ "$TM" == 'S' ]; then
+    echo -e "\nInstalando temas GTK...\n"
+    for TEMA in ./TemasGTK/*.xz
+    do
+        tar -xf "$TEMA" -C /usr/share/themes/
+    done
+fi
+#################################################################################
+
+######################## Extensiones Gnome ######################################
+read -p "Instalar Extensiones Gnome? (S/N): " EXT
+if [ "$EXT" == 'S' ]; then
+    HOMEDIR=`grep "1000" /etc/passwd | awk -F : '{ print $6 }'`
+    USER=`grep "1000" /etc/passwd | awk -F : '{ print $1 }'`
+    PWD=`pwd`
+    for ARCHIVO in "$PWD"/Extensiones/*.zip
+    do
+        UUID=`unzip -c $ARCHIVO metadata.json | grep uuid | cut -d \" -f4`
+        mkdir -p "$HOMEDIR"/.local/share/gnome-shell/extensions/$UUID
+        unzip -q $ARCHIVO -d "$HOMEDIR"/.local/share/gnome-shell/extensions/$UUID/
+    done
+    chown -R "$USER":"$USER" "$HOMEDIR"/.local/
+fi
+#################################################################################
+
+reboot
